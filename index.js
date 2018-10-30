6cbe81c8f86d332b1117cea537d159bd0297b107
Adán <adantoscano@gmail.com>
2018-10-30 13:05:46 +0100

    beautify and remove unused vars

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 var bodyParser = require("body-parser");
 var secureRoutes = require("./routes/secureRoutes.js");
 var routes = require("./routes/routes.js");
-const port = 3000;
 const mongoose = require('mongoose')
 const UserModel = require('./models/userModel');
 const passport = require('passport');
@@ -14,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes(app);
+mongoose.connect('mongodb://localhost:27017/prueba-tecnica', { useNewUrlParser: true });
 
 const db = mongoose.connect('mongodb://localhost:27017/prueba-tecnica', { useNewUrlParser: true });
 mongoose.connection.on('error', error => console.log(error) );
@@ -36,17 +36,3 @@ app.use(function(err, req, res, next) {
 var server = app.listen(3000, function () {
   console.log("The API is running on port:", server.address().port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
