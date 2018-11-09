80e677016529970f6267918f18b13df365a772f1
javheroli <javierherraizolivas@gmail.com>
2018-11-09 13:50:39 +0100

    Added API GUI using react as frontend

---
+++ public/precache-manifest.86055d570abdd7cde5272d5afd7147a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4124d58c1f88c5efc158",
+    "url": "/static/js/main.4124d58c.chunk.js"
+  },
+  {
+    "revision": "e8aeee86fa71ed2deaa6",
+    "url": "/static/js/1.e8aeee86.chunk.js"
+  },
+  {
+    "revision": "4124d58c1f88c5efc158",
+    "url": "/static/css/main.f69fbde2.chunk.css"
+  },
+  {
+    "revision": "e8aeee86fa71ed2deaa6",
+    "url": "/static/css/1.bbe11a26.chunk.css"
+  },
+  {
+    "revision": "b9a58555c7e6c26a801b73141b2e8cc5",
+    "url": "/index.html"
+  }
+];
